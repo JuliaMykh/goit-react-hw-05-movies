3ce67bd8d689ad47bdc58f59a1c1f114bbf14eb1
Mykhailenko <juliam4433@gmail.com>
2022-10-09 13:49:00 +0200

    update

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,23 +1,23 @@
 // import propTypes from 'prop-types';
 
 
-function MoviesList({ movies }) {
-  return (
-    <div>
-      {movies.map(({ id, title, poster, voteAverage }) => {
-        return (
-          <li
-            key={id}
-            id={id}
-            title={title}
-            poster={poster}
-            voteAverage={voteAverage}
-          />
-        );
-      })}
-    </div>
-  );
-}
+// function MoviesList({ movies }) {
+//   return (
+//     <div>
+//       {movies.map(({ id, title, poster, voteAverage }) => {
+//         return (
+//           <li
+//             key={id}
+//             id={id}
+//             title={title}
+//             poster={poster}
+//             voteAverage={voteAverage}
+//           />
+//         );
+//       })}
+//     </div>
+//   );
+// }
 
 
 
